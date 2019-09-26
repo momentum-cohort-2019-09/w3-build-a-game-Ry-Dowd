a56ead6b9e3f5d0aac71142dc3f560aa1684dab3
Ryan <rydowd@gmail.com>
2019-09-26 17:41:46 -0400

    the player is now properly confined to the canvas. thereis no escape.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 class Game {
     constructor() {
         const canvas = document.querySelector("#canvas")
@@ -11,103 +10,123 @@ class Game {
         this.score = 0
         this.sprites.push(new Player(this, this.size))
         let threshold = .95
-       let tick = () => {
-           console.log("ticktickticktick")
-           this.update(threshold)
-           this.draw(screen,this.size)
-           requestAnimationFrame(tick)
-       } 
-       tick()
+        let tick = () => {
+            console.log("ticktickticktick")
+            this.update(threshold)
+            this.draw(screen, this.size)
+            requestAnimationFrame(tick)
+        }
+        tick()
     }
     addSprite(sprite) {
         this.sprites.push(sprite)
     }
-    draw(screen, size){
-        screen.clearRect(0,0,size.x,size.y)
-        for(let sprite of this.sprites){
+    draw(screen, size) {
+        screen.clearRect(0, 0, size.x, size.y)
+        for (let sprite of this.sprites) {
             this.render(screen, sprite)
         }
     }
-    update(threshold){
-        if (Math.random()>threshold){
+    update(threshold) {
+        if (Math.random() > threshold) {
             this.spawnEnemy()
         }
-        for (let sprite of this.sprites){
+        for (let sprite of this.sprites) {
             sprite.update()
         }
     }
-    render(screen, sprite){
+    render(screen, sprite) {
         screen.fillStyle = sprite.color
-        screen.fillRect(sprite.center.x-sprite.size.x/2,sprite.center.y-sprite.size.y/2,sprite.size.x,sprite.size.y)
+        screen.fillRect(sprite.center.x - sprite.size.x / 2, sprite.center.y - sprite.size.y / 2, sprite.size.x, sprite.size.y)
     }
-    spawnEnemy(){
+    spawnEnemy() {
         let rand = Math.random()
-        let spawnPoint = {x:0,y:0}
-        let offset = Math.floor(Math.random()*this.size.x)
-        if(rand<.25){
+        let spawnPoint = { x: 0, y: 0 }
+        let offset = Math.floor(Math.random() * this.size.x)
+        if (rand < .25) {
             spawnPoint.x += offset
-        }else if (rand <.5){
+        } else if (rand < .5) {
             spawnPoint.y += offset
-        }else if (rand <.75){
+        } else if (rand < .75) {
             spawnPoint.y = this.size.y
             spawnPoint.x += offset
-        }else{
+        } else {
             spawnPoint.x = this.size.x
             spawnPoint.y += offset
         }
-        this.addSprite(new Enemy(this, spawnPoint, .8))
+        this.addSprite(new Enemy(this, spawnPoint, .6 + Math.random() / 2))
     }
 }
 
 class Player {
     constructor(game, size) {
         this.game = game
-        this.center = {x:size.x/2, y:size.y/2}
-        this.size = {x:15,y:15}
+        this.center = { x: size.x / 2, y: size.y / 2 }
+        this.size = { x: 15, y: 15 }
         this.inventory = []
         this.color = "blue"
         this.keyboarder = new Keyboarder()
         window.playerPosition = this.center
     }
-    update(){
-        if (this.keyboarder.isDown(Keyboarder.KEYS.LEFT) || this.keyboarder.isDown(Keyboarder.KEYS.A)){
+    update() {
+        if (this.keyboarder.isDown(Keyboarder.KEYS.LEFT) || this.keyboarder.isDown(Keyboarder.KEYS.A)) {
             this.center.x -= 2
         }
-        if (this.keyboarder.isDown(Keyboarder.KEYS.UP) || this.keyboarder.isDown(Keyboarder.KEYS.W)){
+        if (this.keyboarder.isDown(Keyboarder.KEYS.UP) || this.keyboarder.isDown(Keyboarder.KEYS.W)) {
             this.center.y -= 2
-        } 
-        if (this.keyboarder.isDown(Keyboarder.KEYS.DOWN) || this.keyboarder.isDown(Keyboarder.KEYS.S)){
+        }
+        if (this.keyboarder.isDown(Keyboarder.KEYS.DOWN) || this.keyboarder.isDown(Keyboarder.KEYS.S)) {
             this.center.y += 2
         }
-        if(this.keyboarder.isDown(Keyboarder.KEYS.RIGHT)|| this.keyboarder.isDown(Keyboarder.KEYS.D)){
+        if (this.keyboarder.isDown(Keyboarder.KEYS.RIGHT) || this.keyboarder.isDown(Keyboarder.KEYS.D)) {
             this.center.x += 2
         }
+        if (this.keyboarder.isDown(Keyboarder.KEYS.SPACE)) {
+            this.useItem()
+        }
+        if (this.center.x >= this.game.size.x) {
+            this.center.x = this.game.size.x
+        }
+        if (this.center.y >= this.game.size.y) {
+            this.center.y = this.game.size.y
+        }
+        if (this.center.x <= 0){
+            this.center.x = 0
+        }
+        if(this.center.y <= 0){
+            this.center.y = 0
+        }
+    }
+    useItem(){
+
     }
 }
 class Enemy {
     constructor(game, position, velocity) {
         this.game = game
         this.center = position
-        this.size = {x:10,y:10}
+        this.size = { x: 10, y: 10 }
         this.velocity = velocity
         this.color = "green"
         this.angularVelocity = this.aim(window.playerPosition)
     }
-    aim(player){
-        let theta = Math.atan2(player.y-this.center.y, player.x-this.center.x)
-        let xVel = this.velocity*Math.cos(theta)
-        let yVel = this.velocity*Math.sin(theta)
+    aim(player) {
+        let theta = Math.atan2(player.y - this.center.y, player.x - this.center.x)
+        let xVel = this.velocity * Math.cos(theta)
+        let yVel = this.velocity * Math.sin(theta)
 
-        return {x: xVel, y: yVel}
+        return { x: xVel, y: yVel }
     }
-    update(){
-        this.center = {x:this.center.x + this.angularVelocity.x, y:this.center.y + this.angularVelocity.y}
+    update() {
+        this.center = { x: this.center.x + this.angularVelocity.x, y: this.center.y + this.angularVelocity.y }
         this.angularVelocity = this.aim(window.playerPosition)
     }
 }
 class Powerup {
-    constructor(position, type) {
+    constructor(game, position, type) {
+        this.game = game
         this.position = position
         this.type = type
     }
+
 }
