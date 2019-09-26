6a503b0b27dc70e03609ad398540d4658f6cd72f
Ryan <rydowd@gmail.com>
2019-09-26 17:03:14 -0400

    the zombie horde is alive

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ class Game {
     constructor() {
         const canvas = document.querySelector("#canvas")
         const screen = canvas.getContext('2d')
-        let gameSize = {
+        this.size = {
             x: canvas.width,
             y: canvas.height
         }
         this.sprites = []
         this.score = 0
-        this.sprites.push(new Player(this, gameSize))
-        let threshold = .99
+        this.sprites.push(new Player(this, this.size))
+        let threshold = .95
        let tick = () => {
            console.log("ticktickticktick")
            this.update(threshold)
-           this.draw(screen,gameSize)
+           this.draw(screen,this.size)
            requestAnimationFrame(tick)
        } 
        tick()
@@ -22,46 +22,87 @@ class Game {
     addSprite(sprite) {
         this.sprites.push(sprite)
     }
-    draw(screen, gameSize){
-        screen.clearRect(0,0,gameSize.x,gameSize.y)
+    draw(screen, size){
+        screen.clearRect(0,0,size.x,size.y)
         for(let sprite of this.sprites){
             this.render(screen, sprite)
         }
     }
-    update(){
-
+    update(threshold){
+        if (Math.random()>threshold){
+            this.spawnEnemy()
+        }
+        for (let sprite of this.sprites){
+            sprite.update()
+        }
     }
     render(screen, sprite){
         screen.fillStyle = sprite.color
         screen.fillRect(sprite.center.x-sprite.size.x/2,sprite.center.y-sprite.size.y/2,sprite.size.x,sprite.size.y)
     }
+    spawnEnemy(){
+        let rand = Math.random()
+        let spawnPoint = {x:0,y:0}
+        let offset = Math.floor(Math.random()*this.size.x)
+        if(rand<.25){
+            spawnPoint.x += offset
+        }else if (rand <.5){
+            spawnPoint.y += offset
+        }else if (rand <.75){
+            spawnPoint.y = this.size.y
+            spawnPoint.x += offset
+        }else{
+            spawnPoint.x = this.size.x
+            spawnPoint.y += offset
+        }
+        this.addSprite(new Enemy(this, spawnPoint, .8))
+    }
 }
 
 class Player {
-    constructor(game, gameSize) {
+    constructor(game, size) {
         this.game = game
-        this.center = {x:gameSize.x/2, y:gameSize.y/2}
+        this.center = {x:size.x/2, y:size.y/2}
         this.size = {x:15,y:15}
         this.inventory = []
         this.color = "blue"
+        this.keyboarder = new Keyboarder()
         window.playerPosition = this.center
+    }
+    update(){
+        if (this.keyboarder.isDown(Keyboarder.KEYS.LEFT) || this.keyboarder.isDown(Keyboarder.KEYS.A)){
+            this.center.x -= 2
+        }
+        if (this.keyboarder.isDown(Keyboarder.KEYS.UP) || this.keyboarder.isDown(Keyboarder.KEYS.W)){
+            this.center.y -= 2
+        } 
+        if (this.keyboarder.isDown(Keyboarder.KEYS.DOWN) || this.keyboarder.isDown(Keyboarder.KEYS.S)){
+            this.center.y += 2
+        }
+        if(this.keyboarder.isDown(Keyboarder.KEYS.RIGHT)|| this.keyboarder.isDown(Keyboarder.KEYS.D)){
+            this.center.x += 2
+        }
     }
 }
 class Enemy {
-    constructor(game, position, size, velocity) {
+    constructor(game, position, velocity) {
         this.game = game
-        this.position = position
-        this.size = size
+        this.center = position
+        this.size = {x:10,y:10}
         this.velocity = velocity
-        this.color = "red"
-        this.angularVelocity = this.aim(this.position, window.playerPosition,this.velocity)
+        this.color = "green"
+        this.angularVelocity = this.aim(window.playerPosition)
     }
-    aim(position, playerPosition, velocity){
-        let theta = Math.atan2(playerPosition.y-position.y, playerPosition.x-position.x)
-        let xVel = velocity*Math.cos(theta)
-        let yVel = velocity*Math.sin(theta)
+    aim(player){
+        let theta = Math.atan2(player.y-this.center.y, player.x-this.center.x)
+        let xVel = this.velocity*Math.cos(theta)
+        let yVel = this.velocity*Math.sin(theta)
 
         return {x: xVel, y: yVel}
+    }
+    update(){
+        this.center = {x:this.center.x + this.angularVelocity.x, y:this.center.y + this.angularVelocity.y}
+        this.angularVelocity = this.aim(window.playerPosition)
     }
 }
 class Powerup {
